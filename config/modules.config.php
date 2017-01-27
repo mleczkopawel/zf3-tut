59460f25eedbebb2b434fd29280ab0782de8530c
Paweł Mleczko <mleczko.pawel1@gmail.com>
2017-01-27 10:39:30 +0100

    Naprawa

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,10 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
+    'Zend\Hydrator',
+    'Zend\InputFilter',
+    'Zend\Filter',
     'Zend\Session',
     'Zend\Mvc\Plugin\Prg',
     'Zend\Mvc\Plugin\Identity',
